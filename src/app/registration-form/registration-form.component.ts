ff3d5c0e181e0fc1291d4bf5224a414b889faa3e
Mukesh Singh <mukeshs@mumbai1.corp.citiustech.com>
2018-03-05 12:59:00 +0530

    Added registration form

---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,39 @@
+import {
+    Component,
+    OnInit,
+    ViewChild
+} from '@angular/core';
+import { Signup } from '../models/sign-up';
+
+@Component({
+  selector: 'app-registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.css']
+})
+export class RegistrationFormComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+  
+  model: Signup = new Signup();
+  @ViewChild('f') form: any;
+
+  langs: string[] = [
+    'English',
+    'French',
+    'German',
+  ];
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log("Form Submitted!");
+      this.form.reset();
+    }
+  }
+
+}
